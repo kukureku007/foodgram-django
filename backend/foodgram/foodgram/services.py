ec5d1a9c554c1bfa52abb9c38046124e60c0e222
Michael Rudy <michaelrudy@192.168.1.3>
2022-05-22 17:53:09 +0300

    all models done, demo-base done

--- backend/foodgram/foodgram/services.py
+++ backend/foodgram/foodgram/services.py
@@ -1,25 +1,25 @@
 import csv
 
-
-from recipes.models import Ingredient, Tag, Recipe, Ingredients_in_recipes
+from recipes.models import (
+    Ingredient,
+    Tag, Recipe, Ingredients_in_recipes,
+    Favorite, Shopping_cart
+)
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
 
-# rc.import_model_from_csv(Ingredient, 'demo-base/ingredients.csv')
+# TODO change id to pk
 def import_model_from_csv(klass, file_path):
     with open(file_path) as file:
         reader = csv.reader(file)
         headers = next(reader)
-        # print(headers)
         for row in reader:
-            # print(row)
             obj = klass()
             for count, attribute_name in enumerate(headers):
                 setattr(obj, attribute_name, row[count])
             obj.save()
-            # print('-------------')
     # return klass.objects.all()
 
 
@@ -29,7 +29,7 @@ def import_users_from_csv(file_path):
         headers = next(reader)
         # id, username, password, email - для create_user()
         headers = headers[4:]
-        # print(headers)
+        print(headers)
         for row in reader:
             id_, username, password, email = row[:4]
             row = row[4:]
@@ -38,27 +38,48 @@ def import_users_from_csv(file_path):
                 password=password,
                 email=email
             )
-            # print(user.id)
             user.id = id_
             User.objects.get(username=username).delete()
-            # user.save()
 
             for count, attribute_name in enumerate(headers):
                 setattr(user, attribute_name, row[count])
             user.save()
 
 
+# def import_model_to_model(klass_intermediate, klass_root, klass, file_path):
+    # with open(file_path) as file:
+        # reader = csv.reader(file)
+        # next(reader)
+        # for row in reader:
+            # obj = klass_root.objects.get(pk=row[1])
+            # topping = klass.objects.get(pk=row[2])
+            # eval(f'obj.{related_name}.add(topping)')
+
+            # klass_root.RELATED.add(klass)
+
+
 def import_relations_tags(file_path):
     with open(file_path) as file:
         reader = csv.reader(file)
-        # read headers
+        # считать "в пустоту" headers
         next(reader)
         for row in reader:
-            # print(row)
             recipe_id, tag_id = row[1], row[2]
             recipe = Recipe.objects.get(id=recipe_id)
             tag = Tag.objects.get(id=tag_id)
             recipe.tags.add(tag)
+
+
+def import_subscriptions(file_path):
+    with open(file_path) as file:
+        reader = csv.reader(file)
+        # считать "в пустоту" headers
+        next(reader)
+        for row in reader:
+            from_user_pk, to_user_pk = row[1:]
+            from_user = User.objects.get(pk=from_user_pk)
+            to_user = User.objects.get(pk=to_user_pk)
+            from_user.subscriptions.add(to_user)
 
 
 def import_demo_db_for_sure():
@@ -70,9 +91,11 @@ def import_demo_db_for_sure():
     Recipe.objects.all().delete()
     Ingredient.objects.all().delete()
     Ingredients_in_recipes.objects.all().delete()
+    # удаление связей между рецептами и тегами
     Recipe.tags.through.objects.all().delete()
-    # удаление связей тэго с рецептами
-    # MyModel.relations.through.objects.all().delete()
+    Favorite.objects.all().delete()
+    Shopping_cart.objects.all().delete()
+    User.subscriptions.through.objects.all().delete()
 
     import_users_from_csv('demo-base/users.csv')
     import_model_from_csv(Ingredient, 'demo-base/ingredients.csv')
@@ -83,5 +106,6 @@ def import_demo_db_for_sure():
         'demo-base/igredients_in_recipes.csv'
     )
     import_relations_tags('demo-base/recipe_tags.csv')
-
-    # tags
+    import_model_from_csv(Favorite, 'demo-base/favorites.csv')
+    import_model_from_csv(Shopping_cart, 'demo-base/cart.csv')
+    import_subscriptions('demo-base/subscriptions.csv')
--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser
-# from django.db import models
+from django.db import models
 
 
+# TODO можно подписаться на самого себя!!
 class User(AbstractUser):
-    pass
+    subscriptions = models.ManyToManyField('self', symmetrical=False)
+    # favorites = models.ManyToManyField(Recipe)
+    # cart = models.ManyToManyField(Recipe)
+
+
+# user one-to-one
+# favorites
+# cart
+# User.user1to1.favorites.all()
